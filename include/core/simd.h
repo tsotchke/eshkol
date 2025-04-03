fe6f3c144b64a02bb875566eff44d4e3dcd26b5c
tsotchke <tsotchkele@gmail.com>
2025-04-03 13:14:01 -0400

    correction of architecture condition for compilation

--- include/core/simd.h
+++ include/core/simd.h
@@ -10,7 +10,11 @@
 #define ESHKOL_SIMD_H
 
 #include <stdbool.h>
+
+// Only include SIMD headers on x86/x64 architectures
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || defined(_M_IX86)
 #include <xmmintrin.h>
+#endif
 
 #ifdef __cplusplus
 extern "C" {
